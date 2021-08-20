1fada975123db71af44d8a44bcd9810939588b2b
Aaron <79121360+UnrealApex@users.noreply.github.com>
2021-08-19 20:50:47 -0700

    Document filled with spaces don't update word and character count anymore

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
+// checks if the document is filled only with whitespace
+const whiteSpaceRE = /^\s{1,}$/;
+
 function updateStats() {
-    // updates word and character count
-    // copies the value of our textarea so any modifications we make don't affect it  
-    let inputValue = document.querySelector("textarea").value;
+  // updates word and character count
+  // copies the value of our textarea so any modifications we make don't affect it
+  let inputValue = document.querySelector("textarea").value;
+  // if the document isn't blank and it isn't just filled with spaces, change the word and character count, else make both zero
+  if (inputValue.length !== 0 && !whiteSpaceRE.test(inputValue)) {
     if (inputValue.length !== 0) {
         inputValue = inputValue.replace(/(^\s*)|(\s*$)/gi, "");
         inputValue = inputValue.replace(/[ ]{2,}/gi, " ");
