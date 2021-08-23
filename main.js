9422326aa734fafbb9e25cd863da255827d1aee6
Aaron <79121360+UnrealApex@users.noreply.github.com>
2021-08-23 16:57:12 -0700

    Update main.js

--- main.js
+++ main.js
@@ -7,15 +7,15 @@ function updateStats() {
   let inputValue = document.querySelector("textarea").value;
   // if the document isn't blank and it isn't just filled with spaces, change the word and character count, else make both zero
   if (inputValue.length !== 0 && !whiteSpaceRE.test(inputValue)) {
-    if (inputValue.length !== 0) {
-        inputValue = inputValue.replace(/(^\s*)|(\s*$)/gi, "");
-        inputValue = inputValue.replace(/[ ]{2,}/gi, " ");
-        inputValue = inputValue.replace(/\n /, "\n");
-        document.querySelector("#words").innerHTML = "Words: " + inputValue.split(' ').length;
-        document.querySelector("#characters").innerHTML = "Characters: " + inputValue.length;
+      inputValue = inputValue.replace(/(^\s*)|(\s*$)/gi, "");
+      inputValue = inputValue.replace(/[ ]{2,}/gi, " ");
+      inputValue = inputValue.replace(/\n /, "\n");
+      document.querySelector("#words").innerHTML =
+        "Words: " + inputValue.split(" ").length;
+      document.querySelector("#characters").innerHTML =
+        "Characters: " + inputValue.length;
     } else {
-        document.querySelector("#words").innerHTML = "Words: " + 0;
-        document.querySelector("#characters").innerHTML = "Characters: " + 0;
-    }
+      document.querySelector("#words").innerHTML = "Words: 0";
+      document.querySelector("#characters").innerHTML = "Characters: 0";
+  }
 }
-
